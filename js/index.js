48b1ea2cb9e60121a1a257c3296d69a4b89f81a1
Lianzi <lianziliu.sugar@gmail.com>
2022-10-23 15:58:24 -0400

    like btn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 const list = document.querySelector("#list")
-
+const bookPanel = document.querySelector("#show-panel")
 
 fetch ("http://localhost:3000/books")
 .then (r => r.json())
@@ -13,19 +13,24 @@ fetch ("http://localhost:3000/books")
 })
 
 function  renderBooks(bookObj){
+
     const bookList = document.createElement("li")
+
     bookList.textContent = bookObj.title
     list.append(bookList)
 
-    const bookPanel = document.querySelector("#show-panel")
     bookList.addEventListener("click", ()=> {
 
+        bookPanel.innerHTML = " "
+
+        const bookDiv = document.createElement ("div")
         const bookImg = document.createElement ("img")
         const bookTitle = document.createElement ("h2")
         const bookSubtitle = document.createElement ("h3")
         const pAuthor = document.createElement("p")
         const bookDescription = document.createElement ("h5")
         const likeBtn = document.createElement("button")
+        const userList = document.createElement("ul")
 
 
         bookTitle.textContent = bookObj.title
@@ -35,9 +40,50 @@ function  renderBooks(bookObj){
         bookImg.src = bookObj.img_url
         likeBtn.textContent = "LIKE"
 
-        bookPanel.append(bookImg, bookTitle, bookSubtitle,pAuthor, bookDescription,likeBtn)
+
+        bookDiv.append(bookImg, bookTitle, bookSubtitle,pAuthor, bookDescription, userList,likeBtn)
+        bookPanel.append(bookDiv)
+
+        Array.from(bookObj.users).forEach( (users) =>{
+            const user = document.createElement("li")
+            user.textContent = users.username
+            userList.append(user)
+        })
+
+        likeBtn.addEventListener ("click", ()=>{
+            if (likeBtn.textContent = 'LIKE'){
+                fetch(' http://localhost:3000/users')
+                .then(res=>res.json())
+                .then(data=>{
+
+                    let newLike = data [Math.floor(Math.random() * 10)]
+                    let li = document.createElement('li')
+                    li.textContent += newLike.username
+                    userList.appendChild(li)
+                    bookObj.users = [...bookObj.users, newLike]
+                    addUser(bookObj)
+                })
+
+            }
+        })
     })
-}
+
+
+    const addUser = (user)=>{
+        fetch(`http://localhost:3000/books/${user.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json"
+            },
+            body:JSON.stringify(user)
+        })
+        .then (r => r.json())
+
+    }
+
+} // renderBooks
+
 
 
 
